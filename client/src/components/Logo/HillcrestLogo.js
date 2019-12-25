e2b23c1b5f81cebec0401ea1754d115401778b77
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-12-25 18:48:10 -0500

    Hillcrest Name logo gif added, component created

---
+++ client/src/components/Logo/HillcrestLogo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HillcrestLogo = () => {
+    return (
+      <div>
+        <img src={require ("./NameLogo_transparent.gif")} alt="Hillcrest Charlotte Logo" 
+        style={{ maxHeight: 250, maxWidth: 250 }} />
+      </div>
+    )
+}
+
+export default HillcrestLogo
