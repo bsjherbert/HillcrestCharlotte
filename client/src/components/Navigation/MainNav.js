b9023943f8761ca1f1fd248803ded2725db955d4
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-12-25 18:51:37 -0500

    Main nav area created and tabs imported

---
+++ client/src/components/Navigation/MainNav.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MainNavTabs from './MainNavTabs';
+
+class MainNav extends Component {
+  render() {  
+    return (
+      <AppBar color="default" position="static">
+        <Toolbar disableGutters>
+          <MainNavTabs />
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+export default MainNav;
