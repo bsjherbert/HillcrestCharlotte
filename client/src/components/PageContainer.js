073fecc05b8a209fce22a3b71b4c1b1657141d3f
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-12-31 20:48:24 -0500

    updated styling of carousel

--- client/src/components/PageContainer.js
+++ client/src/components/PageContainer.js
@@ -1,59 +1,55 @@
 import React, { Component } from 'react'
 import Container from '@material-ui/core/Container';
-import MainNav from './Navigation/MainNav';
-
+import IconButton from '@material-ui/core/IconButton';
+import { ChevronLeft, ChevronRight } from '@material-ui/icons';
 import Carousel from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
+
+import MainNav from './Navigation/MainNav';
 import ImageSlide from './ImageSlide';
-import SlideArrow from './SlideArrow';
 import imageSlideProps from '../lib/ImageSlideProps.json';
 
 
 class PageContainer extends Component {
 
   // Map over the ImageSlideProps array to populate the Carousel slides 
- render () {
-   return (
-    <Container maxWidth="lg" className="page-container">
-      <MainNav />
-      <div>
-            <Carousel 
-              autoPlay={3000}
-              animationSpeed={1500}
-              infinite
-              arrowLeft={
-                <SlideArrow
-                  glyph="&#9664;"
-                />
-              }
-              arrowRight={
-                <SlideArrow
-                  glyph="&#9654;"
-                />
-              }
-              addArrowClickHandler        
-            > 
+  render() {
+    return (
+      <Container maxWidth="lg" className="page-container">
+        <MainNav />
+        <div style={{zIndex: 5, marginTop: "6vh"}}>
+          <Carousel
+            autoPlay={3500}
+            animationSpeed={1500}
+            infinite
+            centered="true"
+            stopAutoPlayOnHover="true"
+            arrowLeft={<IconButton aria-label="Slide Reverse"> <ChevronLeft color="secondary" /> </IconButton>}
+            arrowRight={ <IconButton aria-label="Slide Forward"> <ChevronRight color="secondary" /> </IconButton>}
+            addArrowClickHandler
+          >
             {imageSlideProps.map(slide => (
               <ImageSlide
-                id = {slide.id} 
-                key = {slide.id}
-                linkTo = {slide.linkTo}
+                id={slide.id}
+                key={slide.id}
+                linkTo={slide.linkTo}
                 imageText={slide.imageText}
-                url = {slide.url}
-                position= {slide.position}
-                top= {slide.top}
+                padding={slide.padding}
+                margin={slide.margin}
+                url={slide.url}
+                position={slide.position}
+                top={slide.top}
                 left={slide.left}
-                opacity={slide.opacity}
-                height= {slide.height}
+                textAlign={slide.textAlign}
+                height={slide.height}
                 backgroundColor={slide.backgroundColor}
-                color= {slide.color}
-                width= {slide.width}
-                 />
-            ))}  
-           </Carousel>
-      </div>
-    </Container>
-  
+                color={slide.color}
+                width={slide.width}
+              />
+            ))}
+          </Carousel>
+        </div>
+      </Container>
     );
   }
 }
