10e9ce9e082632ae4453dc538314a9d3b9fb0a4e
cbush19 <cbush5400@gmail.com>
2020-01-01 17:24:11 -0500

    commit this

--- client/src/components/SlideArrow/index.js
+++ client/src/components/SlideArrow/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SlideArrow(props) {
     return (
-        <button style={{backgroud:"none", border:"none"}}>
+        <button style={{backgroud:"none", border:"none", position:"relative", left:100}}>
             {props.glyph}
         </button>
 
