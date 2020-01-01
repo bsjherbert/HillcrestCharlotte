32522de22ba79cf722e173359148b6be4297b15f
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2020-01-01 17:08:21 -0500

    reordered imports

--- client/src/components/Navigation/MainNav.js
+++ client/src/components/Navigation/MainNav.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/styles';
 
 
