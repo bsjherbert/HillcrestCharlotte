c6db75c9666f9981c4f35f50bd0bb36469d1abbf
cbush19 <cbush5400@gmail.com>
2020-01-02 18:33:12 -0500

    added back-end for connection card, new mongoose mdoe and controller

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const app = express();
 //-- Mongoose Setup ----------------------------------------------------------
 mongoose.connect(
   process.env.MONGODB_URI ||
-  'mongodb://localhost/HillcrestChurch'
+  'mongodb://localhost/hillcrestchurch'
 )
 mongoose.connection.on('error', err => {
   console.log(`Mongoose connection err:\n${err}`)
