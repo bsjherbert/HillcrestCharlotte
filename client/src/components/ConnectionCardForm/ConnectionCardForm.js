3412df4dbf04e1d61c71a7e67bf7705aba1ff4df
bsjherbert <56240025+bsjherbert@users.noreply.github.com>
2020-01-02 13:58:11 -0500

    Updated connection card

--- client/src/components/ConnectionCardForm/ConnectionCardForm.js
+++ client/src/components/ConnectionCardForm/ConnectionCardForm.js
@@ -1,40 +1,71 @@
 import React, { Component } from "react";
+import PageContainer from "../PageContainer";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/styles";
 
+const styles = theme => ({
+  form: {
+    borderBottomStyle: "solid",
+    borderBottom: 8,
+    borderBottomColor: theme.palette.default.main,
+    position: "relative",
+    top: "3vh",
+    zIndex: 5
+  }
+});
 class ConnectionCardForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "coconut" };
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
 
-  handleSubmit(event) {
-    alert("Your favorite flavor is: " + this.state.value);
-    event.preventDefault();
+    this.setState({
+      [name]: value
+    });
   }
 
   render() {
+    const { classes } = this.props;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Pick your favorite flavor:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="grapefruit">Grapefruit</option>
-            <option value="lime">Lime</option>
-            <option value="coconut">Coconut</option>
-            <option value="mango">Mango</option>
-          </select>
-        </label>
-
-        <input type="submit" value="Submit" />
-      </form>
+      <PageContainer>
+        <form className={classes.form}>
+          <label>
+            Is going:
+            <input
+              name="isGoing"
+              type="checkbox"
+              checked={this.state.isGoing}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Number of guests:
+            <input
+              name="numberOfGuests"
+              type="number"
+              value={this.state.numberOfGuests}
+              onChange={this.handleInputChange}
+            />
+          </label>
+        </form>
+      </PageContainer>
     );
   }
 }
 
-export default ConnectionCardForm;
+ConnectionCardForm.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(ConnectionCardForm);
