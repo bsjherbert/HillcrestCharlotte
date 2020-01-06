1284b3b3d3e2a558abb6f9c018e29baa1cef1d92
Osvaldo Ruiz <owebstudent@gmail.com>
2020-01-06 14:47:14 -0500

    fixing side navbar card

--- client/src/components/Pagetemplate/index.js
+++ client/src/components/Pagetemplate/index.js
@@ -1,37 +1,18 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-
 import Grid from '@material-ui/core/Grid';
 import flower from './images/flower.jpg'
+import sidebar from '../../components/sidebarcard'
 
-import ListSubheader from '@material-ui/core/ListSubheader';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Collapse from '@material-ui/core/Collapse';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import StarBorder from '@material-ui/icons/StarBorder';
-import NestedList from '@material-ui/core'
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    width: '100%',
-    // maxWidth: 360,
-    backgroundColor: theme.palette.background.paper
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
-  nested: {
-    paddingLeft: theme.spacing(4),
-  }
 }));
 
 const  Pagetemplate=(props)=> {
@@ -41,8 +22,7 @@ const  Pagetemplate=(props)=> {
     <div className={classes.root}>
       <Grid container spacing={0}>
         <Grid item xs={4}>
-          side navbar
-          turn into card with list nested list
+          {props.sidebar}
         </Grid>
         <Grid item xs={8}>
           <img src ={flower} width="100%" alt = "plant"/>
@@ -55,57 +35,3 @@ const  Pagetemplate=(props)=> {
 }
 export default Pagetemplate
 
-
-
-
-function NestedList() {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
-
-  const handleClick = () => {
-    setOpen(!open);
-  };
-
-  return (
-    <List
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-      subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
-          Nested List Items
-        </ListSubheader>
-      }
-      className={classes.root}
-    >
-      <ListItem button>
-        <ListItemIcon>
-          <SendIcon />
-        </ListItemIcon>
-        <ListItemText primary="Sent mail" />
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon>
-          <DraftsIcon />
-        </ListItemIcon>
-        <ListItemText primary="Drafts" />
-      </ListItem>
-      <ListItem button onClick={handleClick}>
-        <ListItemIcon>
-          <InboxIcon />
-        </ListItemIcon>
-        <ListItemText primary="Inbox" />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <ListItem button className={classes.nested}>
-            <ListItemIcon>
-              <StarBorder />
-            </ListItemIcon>
-            <ListItemText primary="Starred" />
-          </ListItem>
-        </List>
-      </Collapse>
-    </List>
-  );
-}
