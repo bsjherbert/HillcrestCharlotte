d08e0ac18d466ca0a4e4104e2a730441a86752cd
bsjherbert <bsjherbert@gmail.com>
2020-01-06 22:13:04 -0500

    merging

--- client/src/components/ConnectionCardForm/ConnectionCardForm.js
+++ client/src/components/ConnectionCardForm/ConnectionCardForm.js
@@ -17,12 +17,7 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import FormControl from "@material-ui/core/FormControl";
 import FormLabel from "@material-ui/core/FormLabel";
 import Button from "@material-ui/core/Button";
-<<<<<<< HEAD
-
-
-=======
 import Modal from "../Modals/ConformationModal";
->>>>>>> 35657c2f51225ee1ca02813c480d47acc68813a3
 const styles = theme => ({
   form: {
     textAlign: "center",
@@ -69,16 +64,11 @@ class ConnectionCardForm extends Component {
     e.preventDefault();
     console.log(this.state);
     API.ConnectionCard.create(this.state)
-<<<<<<< HEAD
-      .then(connCard => console.log(connCard))
-      .catch(err => console.log(err));  
-=======
       .then(connCard => {
         console.log(connCard);
         this.setState({ status: true });
       })
       .catch(err => console.log(err));
->>>>>>> 35657c2f51225ee1ca02813c480d47acc68813a3
   };
   render() {
     const { classes } = this.props;
