b3c5f7d7aae939b7e64bc1023ec8526467bb592c
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2020-01-09 22:58:39 -0500

    connection card working again. updated address form fields

--- server/controllers/api/connCardController.js
+++ server/controllers/api/connCardController.js
@@ -3,30 +3,37 @@ const sgMail = require("@sendgrid/mail");
 const db = require("../../models");
 
 connCardController.post("/", (req, res) => {
-  console.log(req.body);
-  db.ConnCard.create(req.body)
-    .then(cc => {
-      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-      const msg = {
-        to: "bsjherbert@gmail.com",
-        from: "connect@hillcrestcharlotte.com",
-        subject: "New Connection Card!",
-        text: `dateCreated: ${req.body.dateCreated}\n
-               name: ${req.body.name}\n
-               address: ${req.body.address.street}\n
-                        ${req.body.address.other}\n
-                        ${req.body.address.city}, ${req.body.address.state} ${req.body.address.zip}\n
-               email: ${req.body.email}\n
-               phone: ${req.body.telephone}\n
-               contactMethod: ${req.body.telephone}\n
-               speakPastor: ${req.body.speakPastor}\n
-               moreInfo: ${req.body.moreInfo}\n
-               questions: ${req.body.questions}\n`
-      };
-      sgMail.send(msg);
-      res.json(cc);
+
+  db.ConnCard.create(req.body).then(cc => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: "lbernadel7@gmail.com",
+      from: {
+        name: "Hillcrest Connection Card - Online",
+        email: "connection@hillcrestcharlotte.com"
+      },
+      subject: "New Connection Card!",
+      text: `dateCreated: ${req.body.dateCreated}\n
+            name: ${req.body.name}\n
+            address: ${req.body.address.street}\n
+                    ${req.body.address.other}\n
+                    ${req.body.address.city}, ${req.body.address.state} ${req.body.address.zip}\n
+            email: ${req.body.email}\n
+            phone: ${req.body.telephone}\n
+            contactMethod: ${req.body.contactMethod}\n
+            speakPastor: ${req.body.speakPastor}\n
+            moreInfo: ${req.body.moreInfo}\n
+            questions: ${req.body.questions}\n`
+    }
+
+    sgMail.send(msg)
+    .then(() => {
+      console.log('Mail sent successfully')
+      res.json(cc)
     })
-    .catch(err => res.json(err));
+    .catch(error => console.error(error.toString()));;
+    
+  }).catch(err => res.json(err));
 });
 
 module.exports = connCardController;
