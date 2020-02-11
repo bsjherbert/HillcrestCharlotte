c54e1673a1086aad207d84cb36083186ec4e9326
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2020-02-11 14:14:27 -0500

    content styling and update

--- client/src/pages/NotFound/NotFound.js
+++ client/src/pages/NotFound/NotFound.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Paper, Typography } from "@material-ui/core";
 
 export default () => (
-  <div className='NotFound'>
-    <h1>Uh-oh!</h1>
-    <p>
-      Sorry, the page you requested cannot be found. <Link to='/'>Return to the home page</Link>.
-    </p>
-  </div>
+  <Paper style={{ padding: "2rem" }}>
+    <Typography component="h1" variant="h2" color="textSecondary">
+      Error 404
+    </Typography>
+    <Typography component="h3" variant="h4" color="error">
+      Oh no!
+    </Typography>
+    <Typography component="p" color="error">
+      Sorry, the page you requested cannot be found. {""}
+      <RouterLink to="/">
+        <Typography component="span" variant="p" color="textSecondary">
+          Return home
+        </Typography>
+      </RouterLink>
+      . Please contact us if you encounter this page multiple times, unexpectedly.
+    </Typography>
+  </Paper>
 );
